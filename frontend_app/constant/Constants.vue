bb3c0b8723ab9e9624ffbd361a729fbb268a73bb
jerryho <jerryjp3914@gmail.com>
2024-11-22 01:16:27 +0800

    chore: change post survey url

--- frontend_app/constant/Constants.vue
+++ frontend_app/constant/Constants.vue
@@ -6,7 +6,7 @@ export default {
   MAX_TEMP: 1.2,
   MIN_TEMP: 0.4,
   DEFAULTS_TEMP: 0.6,
-  POST_SURVEY_URL: 'https://moonbear.herokuapp.com/onlinesurvey/1c605f2b-27e5-4e5e-b60d-596ecf56cd0c/13b7a354-22ec-4566-a6a2-ccb689c5fd98',
+  POST_SURVEY_URL: 'https://moonbear.herokuapp.com/onlinesurvey/fdf3e3ff-d1ae-4569-aca9-a017aff5b300/0a1ee3bd-4ecf-42a4-bc24-481ce6562db4',
   URL_USER_PARAMS: 'PROLIFIC_PID',
   TIME_GAP:10,
   MISSION_TIME: 600,
